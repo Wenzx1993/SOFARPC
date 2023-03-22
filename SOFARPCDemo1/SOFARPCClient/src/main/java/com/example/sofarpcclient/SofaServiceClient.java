6a3d14cc1703c46c0ccdba11bfe247c0e5aa8304
文志勋 <1006380802@qq.com>
2023-03-22 18:04:24 +0800

    1.sofarpc api方式发布服务和引用服务 2.引入注册中心zookeeper

--- SOFARPCDemo1/SOFARPCClient/src/main/java/com/example/sofarpcclient/SofaServiceClient.java
+++ SOFARPCDemo1/SOFARPCClient/src/main/java/com/example/sofarpcclient/SofaServiceClient.java
@@ -1,10 +1,19 @@
 package com.example.sofarpcclient;
 
 import com.alipay.sofa.rpc.config.ConsumerConfig;
+import com.alipay.sofa.rpc.config.RegistryConfig;
 import com.example.sofarpcfacade.SofaService;
 
 public class SofaServiceClient {
     public static void main(String[] args) {
+//        directClient();
+        registryClient();
+    }
+
+    /**
+     * 客户端直连方式
+     */
+    private static void directClient() {
         //客户端
         ConsumerConfig<SofaService> consumerConfig = new ConsumerConfig<SofaService>();
         consumerConfig.setProtocol("bolt") //指定协议
@@ -16,6 +25,28 @@ public class SofaServiceClient {
 
         //测试调用
         sofaService.hello("来呀");
+    }
+
+    /**
+     * 客户端注册中心（zookeeper）方式
+     */
+    private static void registryClient() {
+
+        //注册中心
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setProtocol("zookeeper") //注册中心协议
+                .setAddress("192.168.100.176:2182"); //注册地
+
+        //客户端
+        ConsumerConfig<SofaService> consumerConfig = new ConsumerConfig<SofaService>();
+        consumerConfig.setInterfaceId(SofaService.class.getName())
+                .setRegistry(registryConfig);
+
+        //生成代理
+        SofaService sofaService = consumerConfig.refer();
+
+        //调用
+        sofaService.hello("小飞棍");
 
     }
 }
--- SOFARPCDemo1/SOFARPCProvider/src/main/java/com/example/sofarpcprovider/SofaServiceProviderPublish.java
+++ SOFARPCDemo1/SOFARPCProvider/src/main/java/com/example/sofarpcprovider/SofaServiceProviderPublish.java
@@ -1,12 +1,23 @@
 package com.example.sofarpcprovider;
 
+import com.alipay.sofa.rpc.common.RpcConstants;
 import com.alipay.sofa.rpc.config.ProviderConfig;
+import com.alipay.sofa.rpc.config.RegistryConfig;
 import com.alipay.sofa.rpc.config.ServerConfig;
 import com.example.sofarpcfacade.SofaService;
+import org.apache.curator.RetryPolicy;
 
 public class SofaServiceProviderPublish {
     public static void main(String[] args) {
-        //注册中心（服务地址）
+//        directPublish();
+        registerPublish();
+    }
+
+    /**
+     * 直连服务提供者
+     */
+    private static void directPublish() {
+        //运行容器（服务地址）
         ServerConfig serverConfig = new ServerConfig();
         serverConfig.setProtocol("bolt") //指定协议
                 .setPort(12200) //指定端口
@@ -20,5 +31,31 @@ public class SofaServiceProviderPublish {
 
         //发布服务
         providerConfig.export();
+    }
+
+    /**
+     * 注册中心提供服务着
+     */
+    private static void registerPublish() {
+        //注册中心
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setProtocol(RpcConstants.REGISTRY_PROTOCOL_ZK) //注册中心协议
+                .setAddress("192.168.100.176:2182"); //注册地址
+
+        //运行容器
+        ServerConfig serverConfig =  new ServerConfig();
+        serverConfig.setPort(8803)
+                .setProtocol("bolt");
+
+        //服务发布
+        ProviderConfig<SofaService> providerConfig = new ProviderConfig<SofaService>();
+        providerConfig.setServer(serverConfig)
+                .setRegistry(registryConfig)
+                .setInterfaceId(SofaService.class.getName())
+                .setRef(new SofaServiceProvider());
+
+        //服务发布
+        providerConfig.export();
+
     }
 }
